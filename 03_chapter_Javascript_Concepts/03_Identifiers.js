42abdf6149d5fa7941b0e6b8637d01679ead84e7
Jagadesh <jagadesh0002@gmail.com>
2026-07-16 11:01:38 +0530

    Add Chapter 3 to my Repository

---
+++ 03_chapter_Javascript_Concepts/03_Identifiers.js
@@ -0,0 +1,22 @@
+// Identifiers are the names used to identify variables, functions, class and other user defined names
+// In simple word, name of the container holds value
+
+var ABC =10;
+var abc = 10;
+var _abc = 10;
+var $ = 9;
+
+var a1s =4;
+var abc1299_$ = 10;
+
+// Invalid Variable Names
+
+// var 1ad =10;
+// var ab db = 10;
+// var if = 19;
+
+// Supports Unicode Identifiers
+var தமிழ் = 10;
+var num5 = ஐந்து; // unicode values are not accepted and return not deifned
+console.log(num5);
+console.log(type0f(num5));
