447408fc4de1e03487d58e7137d634326000c6a5
Jagadesh <jagadesh0002@gmail.com>
2026-07-16 18:35:56 +0530

    Example Inter view Questions

---
+++ 04_chapter_Operators/09_Sample_Interview_Questions_Part_1.js
@@ -0,0 +1,24 @@
+console.log(0 == false); // true
+
+console.log(0 === false); // false
+
+console.log(typeof(0)=="number"); // true
+
+console.log(typeof(0) === "number"); // true
+
+console.log(2+"5"); // 25
+
+let a = 2, b= "5", c = a - b;
+console.log(2 - "5"); // -3
+console.log(c);
+console.log(typeof(c));
+console.log(b-a); // 3
+
+
+
+
+
+
+
+
+
