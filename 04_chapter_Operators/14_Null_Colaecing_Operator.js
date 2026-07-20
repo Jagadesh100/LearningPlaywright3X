ae314c8b3b164f97d745041f66d416277c0adcb6
Jagadesh <jagadesh0002@gmail.com>
2026-07-20 14:04:43 +0530

    Null Colaecing Operator

---
+++ 04_chapter_Operators/14_Null_Colaecing_Operator.js
@@ -0,0 +1,3 @@
+let warehouseValue = null;
+let dashboardValue = warehouseValue ?? 3.25; // if refrenced value is null, ?? Null Coleacing Opertaor replaces value present in RHS to variable
+console.log(dashboardValue);
